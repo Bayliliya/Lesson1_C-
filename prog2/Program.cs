6026617ac0be04d831583c003dbce6686cc81644
Liliya <gizat.83@mail.ru>
2022-05-26 11:07:24 +0300

    Проверка является ли число квадратом

---
+++ prog2/Program.cs
@@ -0,0 +1,15 @@
+﻿//программа принимает 2 числа  и проверяет является ли второе число квадратом
+Console.Write("Ведите число 1: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите число 2: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n2 == n1 * n1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+
+    Console.WriteLine("Нет");
+}
+
