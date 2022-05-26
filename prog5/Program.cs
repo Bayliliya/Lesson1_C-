3973c387e937beea1fc29bea4743b1a1b9b9bdc4
Liliya <gizat.83@mail.ru>
2022-05-26 11:56:19 +0300

    Программа выводит числа

---
+++ prog5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывод всех чисел от -N до N
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = -n - 1;
+while (a < n)
+{
+    a += 1;
+    Console.Write(a);
+    Console.Write(' ');
+}
