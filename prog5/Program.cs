e0cdd21da94373853a6c473d0673bb8593091020
Liliya <gizat.83@mail.ru>
2022-05-26 12:04:35 +0300

    Исправление кода от -n

--- prog5/Program.cs
+++ prog5/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Вывод всех чисел от -N до N
 Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int a = -n - 1;
-while (a < n)
+int a = -n;
+while (a <= n)
 {
-    a += 1;
     Console.Write(a);
     Console.Write(' ');
+    a+=1;
 }
