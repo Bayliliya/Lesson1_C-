6d19099221fb62cf07fc880bd3ab206b404e4592
Liliya <gizat.83@mail.ru>
2022-05-26 10:46:20 +0300

    исправление =

--- prog1/Program.cs
+++ prog1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//программа принимает число и выдает его квадрат
+Console.Write ("Ведите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Квадрат числа равен =" + n*n);
+
