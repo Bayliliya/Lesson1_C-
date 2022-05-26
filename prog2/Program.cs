8107367adf3bed32b44998b5a4c096dbcc1ceeff
Liliya <gizat.83@mail.ru>
2022-05-26 11:11:09 +0300

    Исправление орфографии

--- prog2/Program.cs
+++ prog2/Program.cs
@@ -1,7 +1,7 @@
 ﻿//программа принимает 2 числа  и проверяет является ли второе число квадратом
-Console.Write("Ведите число 1: ");
+Console.Write("Введите число 1: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Ведите число 2: ");
+Console.Write("Введите число 2: ");
 int n2 = Convert.ToInt32(Console.ReadLine());
 if (n2 == n1 * n1)
 {
